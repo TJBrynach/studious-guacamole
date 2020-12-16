6f32dd43c0031ff874915f1612913c1c7ef8845e
TJBrynach <57762966+TJBrynach@users.noreply.github.com>
2020-12-16 16:19:33 +0000

    Commit these changes

---
+++ Number_Guessing.py
@@ -0,0 +1,5 @@
+2. Number Guessing
+
+This is one of the simple python projects yet an exciting one. You can even call it a mini-game. Make a program in which the computer randomly chooses a number between 1 to 10, 1 to 100, or any range. Then give users a hint to guess the number. Every time the user guesses wrong, he gets another clue, and his score gets reduced. The clue can be multiples, divisible, greater or smaller, or a combination of all.
+
+You will also need functions to compare the inputted number with the guessed number, to compute the difference between the two, and to check whether an actual number was inputted or not in this python project.
---
+++ README.md
@@ -0,0 +1,4 @@
+Hello!
+
+Im writing this on my command prompt from my laptop, just messing about with github and git, trying to get this up and running. Going to be posting some of my python work up on here.
+
