ede4cd681a2862b9daecae26ee040c9ce668771f
TJBrynach <57762966+TJBrynach@users.noreply.github.com>
2020-12-18 11:53:27 +0000

    First edition complete

--- Number_Guessing.py
+++ Number_Guessing.py
@@ -1,7 +1,4 @@
 ### First Edition
-# Improvements:
-# make it 1 - 100, better hints rather than higher or lower, make it unbreakable, cleaner
-# guessing a random number on a computer
 from random import randint
 
 
@@ -12,22 +9,23 @@ def number_guesser():
         print("Lets get this show on the road, number is being generated between 0-100, you have 5 guesses to get this correct")
         
         num = randint(1,100)
-        guess = int(input("Our number to guess has been randomly generated. What is your first guess?  "))
+        guess = int(input("Our number to guess has been randomly generated. Guess a number between 1 and 100 :  "))
         guess_count = 0
         
         while guess_count <= 6:
             if guess == num:
-                print("Well done, you got it!")
+                print(f'Well done, you got it!, you have correctly guessed {num}!!!')
                 break
+
             elif guess > num:
                 print("Sorry try again. Your guess was too high")
                 guess_count += 1
-                guess = int(input("What is your next guess?  "))
+                guess = int(input("What is your next guess :  "))
 
             elif guess < num:
                 print("Sorry try again, your guess was too low")
                 guess_count += 1
-                guess = int(input("What is your next guess?  "))
+                guess = int(input("What is your next guess :  "))
     else:
         print("Ah well maybe another time")
 
