781c5e8f544d85b3edb5e8c04814f11501a6818a
TJBrynach <57762966+TJBrynach@users.noreply.github.com>
2020-12-18 13:16:48 +0000

    update

--- Reverse_NG.py
+++ Reverse_NG.py
@@ -1,17 +1,14 @@
 # Reverse Number guessing game, User picks a number and its the computers place to guess what it is.
 
 from random import randint
-import time
 
 def guess():
-    print('Hello! Please pick a number between 0 and 100, I will try and guess it :) Are you ready?')
     low = 1
     high = 100
-    x = ''
-    #time.sleep(2)
-   
-    while x[0] != 'x':
-        g = randint(low,high)
+    x = ''  
+    
+    while x[0] != 'c':
+        g = randint(low, high)
         x = input(print(f'Is {g}, Your number (C), Too high (H) or Too low (L) ?')).lower()
         if x[0] == 'h':
             low = g + 1
