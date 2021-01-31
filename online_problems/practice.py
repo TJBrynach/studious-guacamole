4efbc651ae62cb11314d9bc23d0f735c7d205ea4
TJBrynach <57762966+TJBrynach@users.noreply.github.com>
2021-01-31 17:19:01 +0000

    commit

--- online_problems/practice.py
+++ online_problems/practice.py
@@ -58,3 +58,84 @@ def singleNumber(self, nums: List[int]) -> int:
     for i in hash_table:
         if hash_table[i] == 1:
             return i
+
+####################
+#1221. Split a String in Balanced Strings
+# First attempt 32ms, 14.4mb
+
+
+def balancedStringSplit(self, s: str) -> int:
+    # balanced strings are those who have equal quantity of L and R characters.
+    # Given a balanced string s, split it in the max amount of balanced strings. return max amount of b strings
+    # "-RL-RRLL-RL-RL-" returns 4 
+    
+    count = num = 0
+    for char in s:
+        if char == "R":
+            num += 1
+            if num == 0:
+                count += 1
+        elif char == "L":
+            num -= 1
+            if num == 0:
+                count += 1
+    return count
+
+                
+#################
+#1185. Day of the Week
+
+
+def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
+    day_of_week = { 0: "Sunday",
+                    1: "Monday",
+                    2: "Tuesday",
+                    3: "Wednesday",
+                    4: "Thursday",
+                    5: "Friday",
+                    6: "Saturday"   }
+    month_of_year = {   1:  0,
+                2:  3,
+                3:  3,
+                4:  6,
+                5:  1,
+                6:  4,
+                7:  6,
+                8:  2,
+                9:  5,
+                10: 0,
+                11: 3,
+                12: 5   }
+    
+    century = { 17: 4,
+                18: 2,
+                19: 0,
+                20: 6,
+                21: 4,
+                22: 2,
+                23: 0    }
+    yy, cc = int(str(year)[-2:]), int(str(year)[:2])
+    year_code = yy + (yy /4) % 7
+    month_code = month_of_year.get(month)
+    century_code = century.get(cc)
+    leap_year_code = 0
+    if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+        if month <= 2:
+            leap_year_code = 1
+            
+    x = (year_code + month_code + century_code + day - leap_year_code) % 7 
+
+    return day_of_week.get(int(x))
+
+########
+# 326. Power of Three
+
+def isPowerOfThree(self, n: int) -> bool:
+    if n == 1 or n==3:
+        return True
+    else:
+        while n >= 3:
+            n /= 3
+            if n == 3:
+                return True
+        return False
